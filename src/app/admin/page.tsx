19dfef2badcdb468e74eba380e72e81a34c73e6a
Simon Oxford <simonoxford99@gmail.com>
2024-11-07 00:00:59 +0000

    Created card component for dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,15 @@
+import { Card, CardHeader, CardDescription, CardContent } from "@/components/ui/card"
+
 export default function AdminDashboard() {
-  return <h1>Dashboard</h1>
+  return <div className="grid grid-cols-1 md:grid-cols-2
+  lg:grid-cols-3 gap-4">
+    <Card>
+      <CardHeader>Sales</CardHeader>
+      <CardDescription>desc</CardDescription>
+      <CardContent>
+        <p>Text</p>
+      </CardContent>
+    </Card>
+  </div>
 }
+
