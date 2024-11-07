e3c33faaf9ef76bebb9839b8f8cfea74a1ed59da
Simon Oxford <simonoxford99@gmail.com>
2024-11-07 11:02:18 +0000

    Included number formatter + admin

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,15 +1,42 @@
-import { Card, CardHeader, CardDescription, CardContent } from "@/components/ui/card"
+import { Card, CardHeader, CardDescription, CardContent, CardTitle } from "@/components/ui/card"
+import db from "@/db/db"
 
+
+
+async function getSalesData() {
+  const data = await db.order.aggregate({
+    _sum: {pricePaidInPence: true },
+    _count: true
+  })
+
+  return {
+    amount: (data._sum.pricePaidInPence \\ 0) /100,
+    numberOfSales: data._count
+  }
+}
 export default function AdminDashboard() {
-  return <div className="grid grid-cols-1 md:grid-cols-2
+  const salesData = await getSalesData()
+  return ( 
+  <div className="grid grid-cols-1 md:grid-cols-2
   lg:grid-cols-3 gap-4">
-    <Card>
-      <CardHeader>Sales</CardHeader>
-      <CardDescription>desc</CardDescription>
-      <CardContent>
-        <p>Text</p>
-      </CardContent>
-    </Card>
+    <DashboardCard title="Sales" subtitle={salesData.numberOfSales}
+    body={salesData.amount} />
   </div>
+  )
 }
 
+type DashboardCardProps = {
+  title: string
+  subtitle: string
+  body: string
+}
+
+function DashboardCard({title, subtitle, body}: DashboardCardProps) {
+  <Card>
+      <CardHeader>{title}</CardHeader>
+      <CardDescription>{subtitle}</CardDescription>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+}
